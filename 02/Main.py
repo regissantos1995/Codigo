b65ab198b02f76fc480f17d2b97da809ccf2ce8e
Regis <regissantos1995@hotmail.com>
2018-09-20 20:31:16 -0300

    DISTANCIA ENTRE DOIS PONTOS

---
+++ 02/Main.py
@@ -0,0 +1,15 @@
+import math
+
+px1= int(input("digite o valor de px1:"))
+px2= int(input("digite o valor de px2:"))
+py1= int(input("digite o valor de py1:"))
+py2= int(input("digite o valor de py2:"))
+d=math.sqrt ((px1-px2)**2+(py1-py2)**2)
+
+print ("distancia entre os pontos:", d)
+
+
+
+
+
+
