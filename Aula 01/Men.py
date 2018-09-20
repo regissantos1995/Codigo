1a23693ad0d14ea6fbc0976e7f6e4e60d50fc7f0
Regis <regissantos1995@hotmail.com>
2018-09-20 20:13:17 -0300

    ENCONTRAR RAIZES DELTA

--- Aula 01/Men.py
+++ Aula 01/Men.py
@@ -1,2 +1,21 @@
 print("oi, mundo")
 print ("oi, novamente")
+
+import math
+a=int(input("informe valor de a: "))
+b=int(input("informe valor de b: "))
+c=int(input("informe valor de c: "))
+delta=b*b-4*a*c
+print("delta: ", delta)
+if delta >= 0:
+    x1=(-b+math.sqrt(delta))/(2*a)
+    x2=(-b+math.sqrt(delta))/(2*a)
+    print("raiz x1: ",x1)
+    print("raiz x2: ", x2)
+else:
+    print("nao existe raizes reais")
+
+
+
+
+
