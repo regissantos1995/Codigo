b55ab5acc1f4b3af5378d96377b8efe54ed680e0
Regis <regissantos1995@hotmail.com>
2018-09-20 21:34:53 -0300

    maior e menor

---
+++ 04/MAIOR MENOR.py
@@ -0,0 +1,13 @@
+import math
+a = input("informe valor de a: ")
+b = input("informe valor de b: ")
+c = input("informe valor de c: ")
+if a>b>c:
+    print(("o menor numero e: "), a, "o maior numero e: ", c)
+
+if b>c>a:
+    print (("o menor numero e: "), b, "o maior numero e:", c)
+
+if c>b>a:
+    print (("o meno numero e: "), c, "o maior numero e: ", c)
+
